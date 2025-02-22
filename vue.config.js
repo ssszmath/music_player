cb1ad0f718cff9deba4d68519630808a6acf703d
sajad <ssszmath@gmail.com>
2025-02-22 14:04:30 +0330

    change config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/your-repo-name/' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/music-playe/' : '/'
 })
